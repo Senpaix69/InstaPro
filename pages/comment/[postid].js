a97676b20f7eb094f636e2b6940a2a6e4911b4e4
Senpai <raohuraira331.rb@gmail.com>
2022-09-11 22:29:04 +0500

    profile section fixed

--- pages/comment/[postid].js
+++ pages/comment/[postid].js
@@ -78,97 +78,109 @@ const CommentList = () => {
     return (
         <div className={`${!darkTheme ? "dark bg-gray-900" : ""} h-screen overflow-y-scroll scrollbar-hide`}>
             <div className="w-full md:max-w-3xl m-auto dark:text-gray-200">
-                <div className="flex space-x-3 px-3 items-center bg-blue-500 dark:bg-gray-900 text-white h-16">
-                    <ArrowLeftIcon className="h-6 w-6 cursor-pointer" onClick={() => router.back()} />
-                    <h1 className="text-lg font-bold">Comments</h1>
-                </div>
-                {post?.caption &&
-                    <div className="m-3 flex border-b-2 border-gray-600 pb-4 mb-5">
-                        <div className="relative h-10 w-10">
-                            {post.profImg && <Image
-                                loading="eager"
-                                alt="image"
-                                src={post.profImg}
-                                height='40px'
-                                width='40px'
-                                className="rounded-full"
-                            />}
-                        </div>
-                        <div className="ml-3 flex-1 mr-3">
-                            <p className='text-md'>
-                                <span onClick={() => router.push(`/profile/${post.username}`)} className='font-bold cursor-pointer'>{post.username} </span>
-                                {post.caption}
-                            </p>
-                            <Moment fromNow className="text-xs font-semibold text-gray-400">{post.timeStamp?.toDate()}</Moment>
-                        </div>
-                    </div>}
-                {loading ? <Loading /> :
-                    <div className="m-3">
-                        {comments.docs?.map((comment, i) => (
-                            <div key={i} className="mb-5">
-                                <div key={i} className="relative w-full flex">
-                                    <div className="absolute">
-                                        <div className="relative h-10 w-10">
-                                            {comment.data().userImg && <Image
-                                                loading="eager"
-                                                alt="image"
-                                                src={comment.data().userImg}
-                                                height='40px'
-                                                width='40px'
-                                                className="rounded-full"
-                                            />}
-                                        </div>
-                                    </div>
-                                    <div className="ml-12 flex-1 mr-3">
-                                        <p className='text-sm'>
-                                            <span onClick={() => router.push(`/profile/${comment.data().username}`)} className='font-bold cursor-pointer'>{comment.data().username} </span>
-                                            {comment.data().comment}
-                                        </p>
-                                    </div>
-                                    <HeartIcon className="h-5 w-5 btn" />
-                                </div>
-                                <div className="mt-1 text-xs text-gray-400 px-12 flex space-x-3 font-semibold">
-                                    <Moment fromNow>{comment.data().timeStamp?.toDate()}</Moment>
-                                    <button onClick={() => triggerUsername(comment, comment.data().username)}>Reply</button>
-                                    {comment.data().username === session?.user.username && <button onClick={() => deleteComment(comment.id)}>Delete</button>}
-                                </div>
-                                {comment.data().subcomments?.map((subCom, index) => (
-                                    <div key={index} className="ml-14 mt-5">
-                                        <div className="w-full flex relative">
-                                            <div className="absolute">
-                                                <div className="relative h-9 w-9">
-                                                    {comment.data().userImg && <Image
-                                                        loading="eager"
-                                                        alt="image"
-                                                        src={subCom.userImg}
-                                                        height='35px'
-                                                        width='35px'
-                                                        className="rounded-full"
-                                                    />}
-                                                </div>
-                                            </div>
-                                            <div className="flex-1 ml-11 mr-3">
-                                                <p className='text-sm'>
-                                                    <span onClick={() => router.push(`/profile/${subCom.username}`)} className='font-bold cursor-pointer'>{subCom.username} </span>
-                                                    {subCom.comment}
-                                                </p>
-                                            </div>
-                                            <HeartIcon className="h-5 w-5 btn" />
-                                        </div>
-                                        <div className="mt-1 text-xs text-gray-400 px-11 flex space-x-3 font-semibold">
-                                            <Moment fromNow>{subCom.timeStamp?.toDate()}</Moment>
-                                            <button onClick={() => triggerUsername(comment, subCom.username)}>Reply</button>
-                                            {subCom.username === session?.user.username && <button onClick={() => deleteSubComment(comment, index)}>Delete</button>}
-                                        </div>
-                                    </div>))}
+
+                {/* comments header */}
+                <section className="absolute top-0 z-40 w-full md:max-w-3xl">
+                    <div className="flex space-x-3 px-3 items-center bg-blue-500 dark:bg-gray-900 text-white h-16">
+                        <ArrowLeftIcon className="h-6 w-6 cursor-pointer" onClick={() => router.back()} />
+                        <h1 className="text-lg font-bold">Comments</h1>
+                    </div>
+                </section>
+
+                {/* comments Body */}
+                <section className="my-20">
+                    {post?.caption &&
+                        <div className="m-3 flex border-b-2 border-gray-600 pb-4 mb-5">
+                            <div className="relative h-10 w-10">
+                                {post.profImg && <Image
+                                    loading="eager"
+                                    alt="image"
+                                    src={post.profImg}
+                                    height='40px'
+                                    width='40px'
+                                    className="rounded-full"
+                                />}
                             </div>
-                        ))}
-                    </div>}
-                <form className='flex items-center py-2 px-4 absolute bottom-0 dark:bg-gray-900 w-full md:max-w-3xl'>
-                    <EmojiHappyIcon className='h-7 dark:text-gray-200' />
-                    <input value={comment} onChange={(e) => setComment(e.target.value)} className='border-none flex-1 outline-none focus:ring-0 dark:bg-transparent dark:placeholder:text-gray-400 dark:text-white' placeholder='add a comment...' type='text' />
-                    <button type='submit' disabled={!comment.trim()} onClick={postComment} className='font-semibold text-blue-500 disabled:text-gray-400'>Post</button>
-                </form>
+                            <div className="ml-3 flex-1 mr-3">
+                                <p className='text-md'>
+                                    <span onClick={() => router.push(`/profile/${post.username}`)} className='font-bold cursor-pointer'>{post.username} </span>
+                                    {post.caption}
+                                </p>
+                                <Moment fromNow className="text-xs font-semibold text-gray-400">{post.timeStamp?.toDate()}</Moment>
+                            </div>
+                        </div>}
+                    {loading ? <Loading /> :
+                        <div className="m-3">
+                            {comments.docs?.map((comment, i) => (
+                                <div key={i} className="mb-5">
+                                    <div key={i} className="relative w-full flex">
+                                        <div className="absolute">
+                                            <div className="relative h-10 w-10">
+                                                {comment.data().userImg && <Image
+                                                    loading="eager"
+                                                    alt="image"
+                                                    src={comment.data().userImg}
+                                                    height='40px'
+                                                    width='40px'
+                                                    className="rounded-full"
+                                                />}
+                                            </div>
+                                        </div>
+                                        <div className="ml-12 flex-1 mr-3">
+                                            <p className='text-sm'>
+                                                <span onClick={() => router.push(`/profile/${comment.data().username}`)} className='font-bold cursor-pointer'>{comment.data().username} </span>
+                                                {comment.data().comment}
+                                            </p>
+                                        </div>
+                                        <HeartIcon className="h-5 w-5 btn" />
+                                    </div>
+                                    <div className="mt-1 text-xs text-gray-400 px-12 flex space-x-3 font-semibold">
+                                        <Moment fromNow>{comment.data().timeStamp?.toDate()}</Moment>
+                                        <button onClick={() => triggerUsername(comment, comment.data().username)}>Reply</button>
+                                        {comment.data().username === session?.user.username && <button onClick={() => deleteComment(comment.id)}>Delete</button>}
+                                    </div>
+                                    {comment.data().subcomments?.map((subCom, index) => (
+                                        <div key={index} className="ml-14 mt-5">
+                                            <div className="w-full flex relative">
+                                                <div className="absolute">
+                                                    <div className="relative h-9 w-9">
+                                                        {comment.data().userImg && <Image
+                                                            loading="eager"
+                                                            alt="image"
+                                                            src={subCom.userImg}
+                                                            height='35px'
+                                                            width='35px'
+                                                            className="rounded-full"
+                                                        />}
+                                                    </div>
+                                                </div>
+                                                <div className="flex-1 ml-11 mr-3">
+                                                    <p className='text-sm'>
+                                                        <span onClick={() => router.push(`/profile/${subCom.username}`)} className='font-bold cursor-pointer'>{subCom.username} </span>
+                                                        {subCom.comment}
+                                                    </p>
+                                                </div>
+                                                <HeartIcon className="h-5 w-5 btn" />
+                                            </div>
+                                            <div className="mt-1 text-xs text-gray-400 px-11 flex space-x-3 font-semibold">
+                                                <Moment fromNow>{subCom.timeStamp?.toDate()}</Moment>
+                                                <button onClick={() => triggerUsername(comment, subCom.username)}>Reply</button>
+                                                {subCom.username === session?.user.username && <button onClick={() => deleteSubComment(comment, index)}>Delete</button>}
+                                            </div>
+                                        </div>))}
+                                </div>
+                            ))}
+                        </div>}
+                </section>
+
+                {/* comments bottom */}
+                <section>
+                    <form className='flex items-center py-2 px-4 absolute bottom-0 z-50 bg-white dark:bg-gray-900 w-full md:max-w-3xl'>
+                        <EmojiHappyIcon className='h-7 dark:text-gray-200' />
+                        <input value={comment} onChange={(e) => setComment(e.target.value)} className='border-none flex-1 outline-none focus:ring-0 dark:bg-transparent dark:placeholder:text-gray-400 dark:text-white' placeholder='add a comment...' type='text' />
+                        <button type='submit' disabled={!comment.trim()} onClick={postComment} className='font-semibold text-blue-500 disabled:text-gray-400'>Post</button>
+                    </form>
+                </section>
             </div>
         </div>
     )
--- pages/like/[postid].js
+++ pages/like/[postid].js
@@ -19,44 +19,52 @@ const LikeList = () => {
     return (
         <div className={`${!darkTheme ? "dark bg-gray-900" : ""} h-screen overflow-y-scroll scrollbar-hide`}>
             <div className="w-full md:max-w-3xl m-auto dark:text-gray-200">
-                <div className="flex space-x-3 px-3 items-center bg-blue-500 dark:bg-gray-900 text-white h-16">
-                    <ArrowLeftIcon className="h-6 w-6 cursor-pointer" onClick={() => router.back()} />
-                    <h1 className="text-lg font-bold">Likes</h1>
-                </div>
-                <div className="mx-3 mt-5 flex">
-                    <div className="flex items-center space-x-3 m-auto h-9 bg-slate-100 dark:bg-gray-700 rounded-lg p-3 w-full text-sm md:w-[60%]">
-                        <SearchIcon className="h-4 w-4" />
-                        <input className="bg-transparent outline-none focus:ring-0" placeholder="Search" />
+
+                {/* likes header */}
+                <section className="absolute top-0 z-40 w-full md:max-w-3xl bg-white dark:bg-gray-900">
+                    <div className="flex space-x-3 px-3 items-center bg-blue-500 dark:bg-gray-900 text-white h-16">
+                        <ArrowLeftIcon className="h-6 w-6 cursor-pointer" onClick={() => router.back()} />
+                        <h1 className="text-lg font-bold">Likes</h1>
                     </div>
-                </div>
-                <div className="mx-3 mt-5 flex justify-between items-center mb-2">
-                    <h1 className="font-semibold">LIKED BY</h1>
-                    <p className="text-gray-400 text-sm">{likes?.length} {likes?.length === 1 ? "Like" : "Likes"}</p>
-                </div>
-                <div className="mx-3 border-b-2 border-gray-500"></div>
-                {loading ? <Loading /> :
-                    <div className="mx-3">
-                        {likes?.map((like, i) => (
-                            <div key={i} className="w-full flex justify-between items-center">
-                                <div className="relative h-16 flex items-center w-full">
-                                    {like.userImg && <Image
-                                        loading="eager"
-                                        alt="image"
-                                        src={like.userImg}
-                                        height='40px'
-                                        width='40px'
-                                        className="rounded-full"
-                                    />}
-                                    <div className="ml-3">
-                                        <h1 onClick={() => router.push(`/profile/${like.username}`)} className="font-bold mt-1 cursor-pointer">{like.username}</h1>
-                                        {like.timeStamp &&
-                                            <Moment className="text-gray-400 text-xs align-text-top" fromNow>{like.timeStamp.toDate()}</Moment>}
+
+                    <div className="mx-3 mt-5 flex">
+                        <div className="flex items-center space-x-3 m-auto h-9 bg-slate-100 dark:bg-gray-700 rounded-lg p-3 w-full text-sm md:w-[60%]">
+                            <SearchIcon className="h-4 w-4" />
+                            <input className="bg-transparent outline-none focus:ring-0" placeholder="Search" />
+                        </div>
+                    </div>
+                    <div className="mx-3 mt-5 flex justify-between items-center mb-2">
+                        <h1 className="font-semibold">LIKED BY</h1>
+                        <p className="text-gray-400 text-sm">{likes?.length} {likes?.length === 1 ? "Like" : "Likes"}</p>
+                    </div>
+                    <div className="mx-3 border-b-2 border-gray-500"></div>
+                </section>
+
+                <section className="mt-44">
+                    {loading ? <Loading /> :
+                        <div className="mx-3">
+                            {likes?.map((like, i) => (
+                                <div key={i} className="w-full flex justify-between items-center">
+                                    <div className="relative h-16 flex items-center w-full">
+                                        {like.userImg && <Image
+                                            loading="eager"
+                                            alt="image"
+                                            src={like.userImg}
+                                            height='40px'
+                                            width='40px'
+                                            className="rounded-full"
+                                        />}
+                                        <div className="ml-3">
+                                            <h1 onClick={() => router.push(`/profile/${like.username}`)} className="font-bold mt-1 cursor-pointer">{like.username}</h1>
+                                            {like.timeStamp &&
+                                                <Moment className="text-gray-400 text-xs align-text-top" fromNow>{like.timeStamp.toDate()}</Moment>}
+                                        </div>
                                     </div>
+                                    <button className="bg-blue-500 py-1 px-6 text-sm font-semibold rounded-md text-white">Follow</button>
                                 </div>
-                                <button className="bg-blue-500 py-1 px-6 text-sm font-semibold rounded-md text-white">Follow</button>
-                            </div>
-                        ))}
-                    </div>}
+                            ))}
+                        </div>}
+                </section>
             </div>
         </div>
     )
