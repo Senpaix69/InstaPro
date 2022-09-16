52d75dfc83b5d0927d440c1bc2760dfadc28226a
Senpai <raohuraira331.rb@gmail.com>
2022-09-16 21:58:10 +0500

    Notifications Check

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,24 @@ import Header from "../components/Header";
 import Feed from "../components/Feed";
 import Model from "../components/Model";
 import { useSession } from "next-auth/react";
+import OneSignalReact from "react-onesignal";
 import { doc, setDoc, serverTimestamp, getDoc } from 'firebase/firestore';
 import { db } from "../firebase";
 import { useRecoilState } from 'recoil';
 import { themeState } from "../atoms/theme";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const { data: session } = useSession();
   const [darkMode, setDarkMode] = useRecoilState(themeState);
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    OneSignalReact.init({ appId: "9113e602-24c9-47e1-b77f-1582ae760a00" }
+    ).then(() => {
+      setInitialized(true);
+    }), []
+  })
 
   useEffect(() => {
     const theme = JSON.parse(localStorage.getItem('theme'))
