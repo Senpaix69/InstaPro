9875333b1b0221ccb109c774bbef29b47efb9297
Senpai <raohuraira331.rb@gmail.com>
2022-09-17 22:45:50 +0500

    testing

--- components/OneSignal.js
+++ components/OneSignal.js
@@ -3,5 +3,5 @@ import OneSignalReact from "react-onesignal";
 export default async function runOneSignal() {
   // localhost: appId: "9113e602-24c9-47e1-b77f-1582ae760a00"
   // instaPro: appId: 70e7d3f0-737c-40ed-8025-a810a636bc14
-  await OneSignalReact.init({ appId: '9113e602-24c9-47e1-b77f-1582ae760a00', allowLocalhostAsSecureOrigin: true });
+  await OneSignalReact.init({ appId: '70e7d3f0-737c-40ed-8025-a810a636bc14', allowLocalhostAsSecureOrigin: true });
 }
