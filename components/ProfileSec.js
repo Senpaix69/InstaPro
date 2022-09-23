a9931ac32029ce65faed1faaaffd99b159cd1f9c
Senpai <raohuraira331.rb@gmail.com>
2022-09-23 21:18:07 +0500

    added react-toaster

--- components/ProfileSec.js
+++ components/ProfileSec.js
@@ -1,135 +1,255 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { db } from "../firebase";
-import { doc, getDoc, updateDoc, serverTimestamp, setDoc, deleteDoc } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  updateDoc,
+  serverTimestamp,
+  setDoc,
+  deleteDoc,
+} from "firebase/firestore";
 
-const ProfileSec = ({ posts, session, profile, setShowFollowers, setShowFollowings, followers, followings }) => {
-    const [textBio, setTextBio] = useState('');
-    const [textName, setTextName] = useState('');
-    const [hasFollowed, setHasFollowed] = useState(false);
-    const [followYou, setFollowYou] = useState(false);
-    const [editProf, setEditProf] = useState(false);
-    const [user, setUser] = useState({});
-    const [bio, setBio] = useState('');
-    const [name, setName] = useState('');
+const ProfileSec = ({
+  posts,
+  showFollowers,
+  showFollowings,
+  session,
+  profile,
+  setShowFollowers,
+  setShowFollowings,
+  followers,
+  followings,
+}) => {
+  const [textBio, setTextBio] = useState("");
+  const [textName, setTextName] = useState("");
+  const [hasFollowed, setHasFollowed] = useState(false);
+  const [followYou, setFollowYou] = useState(false);
+  const [editProf, setEditProf] = useState(false);
+  const [user, setUser] = useState({});
+  const [bio, setBio] = useState("");
+  const [name, setName] = useState("");
 
-    useEffect(() => {
-        getDoc(doc(db, `profile/${profile}`)).then((userData) => {
-            setBio(userData.data()?.bio ? userData.data()?.bio : "Bio");
-            setName(userData.data()?.fullname ? userData.data()?.fullname : "Name");
-            setUser(userData.data());
-        })
-    }, [profile])
+  useEffect(() => {
+    getDoc(doc(db, `profile/${profile}`)).then((userData) => {
+      setBio(userData.data()?.bio ? userData.data()?.bio : "Bio");
+      setName(userData.data()?.fullname ? userData.data()?.fullname : "Name");
+      setUser(userData.data());
+    });
+  }, [profile]);
 
-    const followUser = async () => {
-        await setDoc(doc(db, `profile/${profile}/followers/${session.user.username}`), {
-            username: session.user.username,
-            profImg: session.user.image,
-            timeStamp: serverTimestamp()
-        })
-        await setDoc(doc(db, `profile/${session.user.username}/followings/${profile}`), {
-            username: user.username,
-            profImg: user.profImg,
-            timeStamp: serverTimestamp()
-        })
+  const followUser = async () => {
+    const load = toast.loading("Following...", { position: "top-center" });
+    await setDoc(
+      doc(db, `profile/${profile}/followers/${session.user.username}`),
+      {
+        username: session.user.username,
+        profImg: session.user.image,
+        timeStamp: serverTimestamp(),
+      }
+    );
+    await setDoc(
+      doc(db, `profile/${session.user.username}/followings/${profile}`),
+      {
+        username: user.username,
+        profImg: user.profImg,
+        timeStamp: serverTimestamp(),
+      }
+    );
+    toast.update(load, {
+      render: "Followed Successfully 😇",
+      position: "top-center",
+      type: "success",
+      isLoading: false,
+      autoClose: true,
+    });
+  };
+
+  const unFollowUser = async () => {
+    if (confirm(`Do you really want to unfollow: ${profile}`)) {
+      const load = toast.loading("unfollowing...", { position: "top-center" });
+      await deleteDoc(
+        doc(db, `profile/${profile}/followers/${session.user.username}`)
+      );
+      await deleteDoc(
+        doc(db, `profile/${session.user.username}/followings/${profile}`)
+      );
+      toast.update(load, {
+        render: "Unfollowed Successfully 😇",
+        position: "top-center",
+        type: "success",
+        isLoading: false,
+        autoClose: true,
+      });
     }
+  };
 
-    const unFollowUser = async () => {
-        if (confirm(`Do you really want to unfollow: ${profile}`)) {
-            await deleteDoc(doc(db, `profile/${profile}/followers/${session.user.username}`));
-            await deleteDoc(doc(db, `profile/${session.user.username}/followings/${profile}`));
-        }
+  const saveEditing = async () => {
+    if (textBio.trim().length > 0 && textName.trim().length > 0) {
+      await updateDoc(doc(db, "profile", profile), {
+        bio: textBio,
+        fullname: textName,
+      });
+      setBio(textBio);
+      setName(textName);
     }
+    setEditProf(false);
+  };
 
-    const saveEditing = async () => {
-        if (textBio.trim().length > 0 && textName.trim().length > 0) {
-            await updateDoc(doc(db, "profile", profile), {
-                bio: textBio,
-                fullname: textName,
-            })
-            setBio(textBio)
-            setName(textName)
-        }
-        setEditProf(false);
-    }
+  useEffect(() => {
+    setHasFollowed(
+      followers?.findIndex(
+        (user) => user.username === session?.user.username
+      ) !== -1
+    );
+    setFollowYou(
+      followings?.findIndex(
+        (user) => user.username === session?.user.username
+      ) !== -1
+    );
+  }, [followers, session]);
 
-    useEffect(() => {
-        setHasFollowed(followers?.findIndex((user) => user.username === session?.user.username) !== -1)
-        setFollowYou(followings?.findIndex((user) => user.username === session?.user.username) !== -1)
-    }, [followers, session])
+  const cancelEditing = () => {
+    setTextBio = "";
+    setTextName = "";
+    setEditProf(false);
+  };
 
-    const cancelEditing = () => {
-        setTextBio = "";
-        setTextName = "";
-        setEditProf(false);
-    }
-
-    return (
-        <div className="relative w-full p-1 px-3 flex flex-col">
-            <div className="flex px-2 relative md:justify-center">
-                <div className="relative h-20 w-20 md:h-24 md:w-24">
-                    {profile === session?.user.username ? <Image
-                        src={session.user.image}
-                        layout="fill"
-                        loading="eager"
-                        alt="profile"
-                        className="rounded-full"
-                    /> :
-                        <Image
-                            src={user?.profImg ? user.profImg : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTagSIpNvrU1N3lvzrWDoSf5oJzABckJ3HbMw&usqp=CAU"}
-                            layout="fill"
-                            loading="eager"
-                            alt="profile"
-                            className="rounded-full"
-                        />}
-                </div>
-                <div className="absolute top-0 right-0 flex w-64 xl:w-80 ml-10 justify-between md:max-w-2xl mt-5 px-4 text-lg">
-                    <button className="flex flex-col items-center">
-                        <p className="font-bold">{posts}</p>
-                        <p className="text-sm mt-1 text-gray-200">Posts</p>
-                    </button>
-                    <button onClick={() => setShowFollowers(true)} className="flex flex-col items-center">
-                        <p className="font-bold">{followers ? followers.length : 0}</p>
-                        <p className="text-sm mt-1 text-gray-200">Followers</p>
-                    </button>
-                    <button onClick={() => setShowFollowings(true)} className="flex flex-col items-center">
-                        <p className="font-bold">{followings ? followings.length : 0}</p>
-                        <p className="text-sm mt-1 text-gray-200">Followings</p>
-                    </button>
-                    {profile !== session.user.username && followings && <div className="bg-gray-100 border border-gray-700 dark:bg-black text-sm text-center w-[226px] xl:w-[290px] py-1 rounded-md absolute -bottom-10 font-semibold">
-                        <span className={`${followYou ? "text-green-500" : "text-red-500"}`}>{followYou ? "Follows You" : "Not Follows You"}</span>
-                    </div>}
-                </div>
-            </div>
-
-            {!editProf &&
-                <div className="mt-1 flex flex-col md:items-center">
-                    <h1 className="font-semibold text-sm">{profile === session?.user.username ? session?.user?.username : user?.username}</h1>
-                    <h1 className="font-semibold text-lg"><span className="text-gray-400">~</span> {name}</h1>
-                    <p className="text-sm text-gray-200">{bio}</p>
-                </div>}
-
-            {(profile === session.user.username) ?
-                <button onClick={() => setEditProf(true)} hidden={editProf} className="w-full max-w-xl mx-auto mt-8 py-1 dark:bg-gray-700 border border-gray-700 rounded-2xl dark:hover:bg-gray-600 bg-blue-500 text-white font-semibold shadow-sm">Edit Profile</button> :
-                <button onClick={!hasFollowed ? followUser : unFollowUser} className="w-full max-w-xl mx-auto mt-8 py-1 dark:bg-gray-700 border border-gray-700 rounded-2xl dark:hover:bg-gray-600 bg-blue-500 text-white font-semibold shadow-sm">{hasFollowed ? "Unfollow" : "Follow"}</button>}
-
-            {editProf &&
-                <div className="mt-5 w-full md:max-w-6xl relative">
-                    <p className="text-xs ml-3">Name</p>
-                    <input className="bg-transparent border-none focus:ring-0 w-full" type='text' placeholder="Enter name" value={textName} onChange={(e) => setTextName(e.target.value)} />
-                    <div className="border-b-2 ml-3 mr-3"></div>
-                    <p className="mt-5 text-xs ml-3">Bio</p>
-                    <input className="bg-transparent border-none focus:ring-0 w-full" type='text' placeholder="Enter bio" value={textBio} onChange={(e) => setTextBio(e.target.value)} />
-                    <div className="border-b-2 ml-3 mr-3"></div>
-                    <div className="relative h-10">
-                        <div className="flex space-x-4 absolute bottom-1 right-3 text-white text-sm font-semibold">
-                            <button onClick={saveEditing} className="bg-blue-500 w-20 h-7 rounded-lg">Save</button>
-                            <button onClick={cancelEditing} className="bg-gray-500 w-20 h-7 rounded-lg">Cancel</button>
-                        </div>
-                    </div>
-                </div>}
+  return (
+    <div
+      className={`relative w-full p-1 px-3 ${
+        showFollowers || showFollowings ? "hidden" : "flex"
+      } flex-col`}
+    >
+      <div className="flex px-2 relative md:justify-center">
+        <div className="relative h-20 w-20 md:h-24 md:w-24">
+          {profile === session?.user.username ? (
+            <Image
+              src={session.user.image}
+              layout="fill"
+              loading="eager"
+              alt="profile"
+              className="rounded-full"
+            />
+          ) : (
+            <Image
+              src={
+                user?.profImg
+                  ? user.profImg
+                  : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTagSIpNvrU1N3lvzrWDoSf5oJzABckJ3HbMw&usqp=CAU"
+              }
+              layout="fill"
+              loading="eager"
+              alt="profile"
+              className="rounded-full"
+            />
+          )}
         </div>
-    )
-}
+        <div className="absolute top-0 right-0 flex w-64 xl:w-80 ml-10 justify-between md:max-w-2xl mt-5 px-4 text-lg">
+          <button className="flex flex-col items-center">
+            <p className="font-bold">{posts}</p>
+            <p className="text-sm mt-1 text-gray-200">Posts</p>
+          </button>
+          <button
+            onClick={() => setShowFollowers(true)}
+            className="flex flex-col items-center"
+          >
+            <p className="font-bold">{followers ? followers.length : 0}</p>
+            <p className="text-sm mt-1 text-gray-200">Followers</p>
+          </button>
+          <button
+            onClick={() => setShowFollowings(true)}
+            className="flex flex-col items-center"
+          >
+            <p className="font-bold">{followings ? followings.length : 0}</p>
+            <p className="text-sm mt-1 text-gray-200">Followings</p>
+          </button>
+          {profile !== session.user.username && followings && (
+            <div className="bg-gray-100 border border-gray-700 dark:bg-black text-sm text-center w-[226px] xl:w-[290px] py-1 rounded-md absolute -bottom-10 font-semibold">
+              <span
+                className={`${followYou ? "text-green-500" : "text-red-500"}`}
+              >
+                {followYou ? "Follows You" : "Not Follows You"}
+              </span>
+            </div>
+          )}
+        </div>
+      </div>
+
+      {!editProf && (
+        <div className="mt-1 flex flex-col md:items-center">
+          <h1 className="font-semibold text-sm">
+            {profile === session?.user.username
+              ? session?.user?.username
+              : user?.username}
+          </h1>
+          <h1 className="font-semibold text-lg">
+            <span className="text-gray-400">~</span> {name}
+          </h1>
+          <p className="text-sm text-gray-200">{bio}</p>
+        </div>
+      )}
+
+      {profile === session.user.username ? (
+        <button
+          onClick={() => setEditProf(true)}
+          hidden={editProf}
+          className="w-full max-w-xl mx-auto mt-8 py-1 dark:bg-gray-700 border border-gray-700 rounded-2xl dark:hover:bg-gray-600 bg-blue-500 text-white font-semibold shadow-sm"
+        >
+          Edit Profile
+        </button>
+      ) : (
+        <button
+          onClick={!hasFollowed ? followUser : unFollowUser}
+          className="w-full max-w-xl mx-auto mt-8 py-1 dark:bg-gray-700 border border-gray-700 rounded-2xl dark:hover:bg-gray-600 bg-blue-500 text-white font-semibold shadow-sm"
+        >
+          {hasFollowed ? "Unfollow" : "Follow"}
+        </button>
+      )}
+
+      {editProf && (
+        <div className="mt-5 w-full md:max-w-6xl relative">
+          <p className="text-xs ml-3">Name</p>
+          <input
+            className="bg-transparent border-none focus:ring-0 w-full"
+            type="text"
+            placeholder="Enter name"
+            value={textName}
+            onChange={(e) => setTextName(e.target.value)}
+          />
+          <div className="border-b-2 ml-3 mr-3"></div>
+          <p className="mt-5 text-xs ml-3">Bio</p>
+          <input
+            className="bg-transparent border-none focus:ring-0 w-full"
+            type="text"
+            placeholder="Enter bio"
+            value={textBio}
+            onChange={(e) => setTextBio(e.target.value)}
+          />
+          <div className="border-b-2 ml-3 mr-3"></div>
+          <div className="relative h-10">
+            <div className="flex space-x-4 absolute bottom-1 right-3 text-white text-sm font-semibold">
+              <button
+                onClick={saveEditing}
+                className="bg-blue-500 w-20 h-7 rounded-lg"
+              >
+                Save
+              </button>
+              <button
+                onClick={cancelEditing}
+                className="bg-gray-500 w-20 h-7 rounded-lg"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      <ToastContainer autoClose={2500} theme="dark" />
+    </div>
+  );
+};
 
 export default ProfileSec;
--- pages/profile/[profile].js
+++ pages/profile/[profile].js
@@ -1,7 +1,7 @@
 import { useSession } from "next-auth/react";
 import { useRecoilState } from "recoil";
-import { themeState } from '../../atoms/theme';
-import Login from '../../pages/login';
+import { themeState } from "../../atoms/theme";
+import Login from "../../pages/login";
 import Header from "../../components/Header";
 import Posts from "../../components/Posts";
 import ProfileSec from "../../components/ProfileSec";
@@ -11,47 +11,93 @@ import { useEffect, useState } from "react";
 import { db } from "../../firebase";
 import { useRouter } from "next/router";
 import { collection, query, orderBy } from "firebase/firestore";
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const Profile = () => {
-    const { data: session } = useSession();
-    const router = useRouter();
-    const [darkMode, setDarkMode] = useRecoilState(themeState);
-    const [view, setView] = useRecoilState(postView);
-    const [totalPosts, setTotalPosts] = useState(0);
-    const [showFollowers, setShowFollowers] = useState(false);
-    const [showFollowings, setShowFollowings] = useState(false);
-    const { profile } = router?.query;
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [darkMode, setDarkMode] = useRecoilState(themeState);
+  const [view, setView] = useRecoilState(postView);
+  const [totalPosts, setTotalPosts] = useState(0);
+  const [showFollowers, setShowFollowers] = useState(false);
+  const [showFollowings, setShowFollowings] = useState(false);
+  const { profile } = router?.query;
 
-    const [followers] = useCollectionData(query(collection(db, `profile/${profile}/followers`), orderBy("timeStamp", "desc")));
-    const [followings, loading] = useCollectionData(query(collection(db, `profile/${profile}/followings`), orderBy("timeStamp", "desc")));
-
-    useEffect(() => {
-        followings ? setShowFollowings(false) : setShowFollowers(false);
-    }, [followings])
-
-    return (
-        <>
-            {session ? <div className={`relative ${darkMode ? "bg-gray-50 " : "dark bg-gray-900"} h-screen overflow-y-scroll scrollbar-hide flex justify-center`}>
-                <div className="max-w-6xl min-w-[380px] dark:text-gray-200 flex-1 overflow-y-scroll scrollbar-hide">
-                    {showFollowers && <FollowList setShowFollowers={setShowFollowers} follow={true} followers={followers} router={router} currUsername={session?.user.username} />}
-                    {showFollowings && <FollowList setShowFollowings={setShowFollowings} followings={followings} router={router} currUsername={session?.user.username} />}
-                    {!showFollowers && !showFollowings && !loading &&
-                        <>
-                            {!view &&
-                                <>
-                                    <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-                                    <ProfileSec posts={totalPosts} session={session} profile={profile} setShowFollowers={setShowFollowers} setShowFollowings={setShowFollowings} followers={followers} followings={followings} />
-                                </>}
-                            <Posts setTotalPosts={setTotalPosts} profile={profile} />
-                            <button disabled={!view} onClick={() => setView(false)} className={`w-full md:max-w-6xl text-white py-2 font-bold uppercase absolute bottom-0 z-30 transition duration-500 overflow-hidden ${view ? "max-h-full bg-blue-500 dark:bg-gray-700" : "max-h-0"}`}>close view</button>
-                        </>}
-                </div>
-            </div > : <Login />
-            }
-        </>
+  const [followers] = useCollectionData(
+    query(
+      collection(db, `profile/${profile}/followers`),
+      orderBy("timeStamp", "desc")
     )
-}
+  );
+  const [followings, loading] = useCollectionData(
+    query(
+      collection(db, `profile/${profile}/followings`),
+      orderBy("timeStamp", "desc")
+    )
+  );
+
+  useEffect(() => {
+    followings ? setShowFollowings(false) : setShowFollowers(false);
+  }, [followings]);
+
+  return (
+    <>
+      {session ? (
+        <div
+          className={`relative ${
+            darkMode ? "bg-gray-50 " : "dark bg-gray-900"
+          } h-screen overflow-y-scroll scrollbar-hide flex justify-center relative`}
+        >
+          <div className="max-w-6xl min-w-[380px] dark:text-gray-200 flex-1 overflow-y-scroll scrollbar-hide">
+            <FollowList
+              setShowFollowers={setShowFollowers}
+              showFollowers={showFollowers}
+              follow={true}
+              followers={followers}
+              router={router}
+              currUsername={session?.user.username}
+            />
+            <FollowList
+              setShowFollowings={setShowFollowings}
+              showFollowings={showFollowings}
+              followings={followings}
+              router={router}
+              currUsername={session?.user.username}
+            />
+            <Header
+              showFollowers={showFollowers}
+              showFollowings={showFollowings}
+              darkMode={darkMode}
+              setDarkMode={setDarkMode}
+            />
+            <ProfileSec
+              posts={totalPosts}
+              session={session}
+              profile={profile}
+              setShowFollowers={setShowFollowers}
+              showFollowers={showFollowers}
+              showFollowings={showFollowings}
+              setShowFollowings={setShowFollowings}
+              followers={followers}
+              followings={followings}
+            />
+            <button
+              hidden={showFollowers || showFollowings ? true : false}
+              className="absolute z-50 bottom-20 ml-3 font-semibold bg-red-800 rounded-[10px] py-1 px-4"
+              onClick={() => setView(!view)}
+            >
+              {view ? "G-View" : "P-View"}
+            </button>
+            {!showFollowers && !showFollowings && !loading && (
+              <Posts setTotalPosts={setTotalPosts} profile={profile} />
+            )}
+          </div>
+        </div>
+      ) : (
+        <Login />
+      )}
+    </>
+  );
+};
 
 export default Profile;
