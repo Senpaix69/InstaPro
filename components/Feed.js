64b6f93a905592697b9b23aca0d0bb0b0c7cd903
Senpai <raohuraira331.rb@gmail.com>
2022-09-24 22:05:09 +0500

    bugs fixed optimizing

--- components/Feed.js
+++ components/Feed.js
@@ -2,10 +2,10 @@ import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
 import Stories from "./Stories";
 import Suggestions from "./Suggestions";
-import { useSession } from 'next-auth/react';
+import { useSession } from "next-auth/react";
 import Login from "../pages/login";
 import Notification from "./Notification";
-import { showUpdate } from '../atoms/showUpdate';
+import { showUpdate } from "../atoms/showUpdate";
 import { useRecoilState } from "recoil";
 import { useEffect, useState } from "react";
 
@@ -20,37 +20,44 @@ const Feed = () => {
       setTimer(false);
       setTimeout(() => {
         setShowNotification(false);
-      }, 1000)
+      }, 1000);
     } else {
       setTimeout(() => {
         setShowNotification(true);
         setTimeout(() => {
-          setTimer(true)
-        }, 300)
-      }, 3000)
+          setTimer(true);
+        }, 300);
+      }, 3000);
     }
-  }, [update])
+  }, [update]);
 
   return (
     <main className="grid grid-cols-1 max-w-6xl xl:grid-cols-3 mx-auto dark:bg-black scroll-smooth">
       {session ? (
         <>
           <section className="col-span-2">
-            <div hidden={!showNotification} className={`m-2 p-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-gray-900 dark:text-gray-300 transition-all duration-1000 ease-in-out ${timer ? "" : "-mt-[400px] sm:-mt-[355px] -translate-y-full"}`}>
-              <span className="font-bold">CAUTION!</span> <span className="font-bold  text-red-500">Isbah BirthDay: 30th September</span>
-              <p className="font-medium">Feed Updates!</p>
-              <p>1: You can now post comments and reply to a comment</p>
-              <p>2: You can delete comments and also delete your sub-comments</p>
-              <p>3: You can check which user has liked the posts</p>
-              <span className="font-medium">Profile Updates!</span>
-              <p>1: You can check other users profile by clicking on usernames</p>
-              <p>2: You can follow and unfollow users</p>
-              <p>3: You can check your own profile</p>
-              <p>4: You can modify you profile name and bio</p>
-              <span className="font-medium">Chat Updates!</span>
-              <p>1: You can now send images in chat</p>
-              <p>2: You can now delete chat and unsend texts</p>
-              <button onClick={() => setUpdate(false)} className="w-full bg-blue-400 rounded-md text-md py-1 shadow-md mt-6 text-white dark:bg-gray-600 font-medium">close</button>
+            <div
+              hidden={!showNotification}
+              className={`m-2 p-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-gray-900 dark:text-gray-300 transition-all duration-1000 ease-in-out ${
+                timer ? "" : "-mt-[150px] sm:-mt-[100px] -translate-y-full"
+              }`}
+            >
+              <span className="font-bold">CAUTION!</span>{" "}
+              <span className="font-bold  text-red-500">
+                Isbah BirthDay: 30th September
+              </span>
+              <p className="font-medium">
+                Note:{" "}
+                <span>
+                  Optimizing The Whole Application Please Cooperate 😊
+                </span>
+              </p>
+              <button
+                onClick={() => setUpdate(false)}
+                className="w-full bg-blue-400 rounded-md text-md py-1 shadow-md mt-6 text-white dark:bg-gray-600 font-medium"
+              >
+                close
+              </button>
             </div>
 
             <Stories />
@@ -68,8 +75,8 @@ const Feed = () => {
       ) : (
         <Login />
       )}
-    </main >
-  )
-}
+    </main>
+  );
+};
 
 export default Feed;
