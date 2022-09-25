5a96a19cbdb1da2c48c7ad4947de74186303ca74
Senpai <raohuraira331.rb@gmail.com>
2022-09-26 01:51:04 +0500

    adding push notifications

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,10 @@
 importScripts("https://js.pusher.com/beams/service-worker.js");
 
-PusherPushNotifications.onNotificationReceived = ({
-  payload,
-  pushEvent,
-  handleNotification,
-}) => {
-  payload.notification.title = "You Got Message!";
-  pushEvent.waitUntil(handleNotification(payload));
-};
+// PusherPushNotifications.onNotificationReceived = ({
+//   payload,
+//   pushEvent,
+//   handleNotification,
+// }) => {
+//   payload.notification.title = "You Got Message!";
+//   pushEvent.waitUntil(handleNotification(payload));
+// };
