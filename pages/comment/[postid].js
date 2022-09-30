733b3168e7373c0b7cec2b3089b2807bdb187f88
Senpai <raohuraira331.rb@gmail.com>
2022-09-30 16:50:14 +0500

    fixed comment section

--- pages/comment/[postid].js
+++ pages/comment/[postid].js
@@ -180,7 +180,7 @@ const CommentList = () => {
                                 alt="profile"
                                 className="rounded-full"
                               />
-                            </div>
+                            </div>{" "}
                           </div>
                         )}{" "}
                       </span>
